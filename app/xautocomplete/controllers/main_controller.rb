397210b2642a835512e07f42317adf107bc68c28
Miguel Angel Alarcos Torrecillas <miguel.alarcos@gmail.com>
2015-08-14 13:01:51 +0200

    added the reference feature

--- app/xautocomplete/controllers/main_controller.rb
+++ app/xautocomplete/controllers/main_controller.rb
@@ -4,6 +4,66 @@ module Xautocomplete
     reactive_accessor :index_  
     reactive_accessor :query  
 
+    def val_f
+      if self.query.blank? and attrs.value
+        if attrs.reference
+          name = attrs.field       #
+          attrs.value.send('_'+name)   #
+        else
+          attrs.value
+        end
+      else
+        self.query
+      end
+    end
+
+    def val_f=(val)
+      self.query = val
+      name = attrs.field #:value
+      collection = attrs.collection # store._authors
+      dct = {}
+      dct[name] = val
+      attrs.value = nil
+      collection.where(dct).first.then do |x|
+        if not x.nil?
+          if attrs.reference
+            attrs.value = x      #
+          else
+            attrs.value = val 
+          end  
+        end
+      end      
+    end
+
+    def show      
+      if self.query.blank?
+        return false
+      end
+      if attrs.reference
+        if autocomplete.first.value and autocomplete.first.value.send('_'+attrs.field) != self.query
+          true
+        else
+          false
+        end
+      else
+        if autocomplete.first.value != self.query
+          true
+        else
+          false
+        end
+      end
+    end 
+
+    def click_(val)
+      name = attrs.field
+      if attrs.reference
+        self.query = val.send('_'+name)
+      else
+        self.query = val
+      end
+      attrs.value = val
+    end
+
     def down e            
       if e.key_code == 38
         self.index_ -= 1
@@ -15,9 +75,17 @@ module Xautocomplete
         collection = attrs.collection # store._authors
         dct = {}
         dct[name] = {"$regex" => '^.*' + self.query + '.*$'}
-        ret = collection.where(dct).all.collect {|x| x.send('_'+name)}      
+        #ret = collection.where(dct).all.collect {|x| x.send('_'+name)}      
+        ret = collection.where(dct).all
         ret.then do |r|
-          self.query = r[self.index_ % r.count]
+        #collection.where(dct).all do |r|
+          count = r.count.value
+          self.query = r[self.index_ % count].send('_'+name).value
+          if attrs.reference
+            attrs.value = r[self.index_ % count].value
+          else
+            attrs.value = r[self.index_ % count].send('_'+name).value
+          end  
         end
       end  
     end
@@ -41,7 +109,11 @@ module Xautocomplete
       collection = attrs.collection #store._authors
       dct = {}
       dct[name] = {"$regex" => '^.*' + self.query + '.*$'}
-      collection.where(dct).all.collect {|x| x.send('_'+name)}            
+      if attrs.reference
+        collection.where(dct).all
+      else
+        collection.where(dct).all.collect {|x| x.send('_'+name)}            
+      end
     end  
 
     def index
